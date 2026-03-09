4a00f00375ed454d2d262f84c163e51458c0bc84
felix <caimin00x@gmail.com>
2026-03-09 21:01:53 +0100

    Fix super-admin editor deployment freshness

--- src/app/super-admin/editor/page.tsx
+++ src/app/super-admin/editor/page.tsx
@@ -1,5 +1,7 @@
 import SuperAdminEditorClient from "./SuperAdminEditorClient";
 
+export const dynamic = "force-dynamic";
+
 export default function SuperAdminEditorPage() {
   return <SuperAdminEditorClient />;
 }
--- src/app/super-admin/page.tsx
+++ src/app/super-admin/page.tsx
@@ -1,5 +1,7 @@
 import SuperAdminClient from "./SuperAdminClient";
 
+export const dynamic = "force-dynamic";
+
 export default function SuperAdminPage() {
   return <SuperAdminClient />;
 }
