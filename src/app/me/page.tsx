e866a11b9e7b50fb240dc16d11a24def50e0513a
felix <caimin00x@gmail.com>
2026-05-02 08:51:48 +0200

    Keep joker labels visible on tool icon

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -1366,32 +1366,32 @@ function ShuangkouToolIcon() {
   return (
     <span className="relative block h-12 w-12" aria-hidden="true">
       <span className={`${cardClass} left-1.5 top-2 rotate-[-12deg]`}>
-        <span className="absolute left-0.5 top-0.5 grid gap-0 text-[4.5px] font-black leading-[0.7] text-red-700">
+        <span className="absolute left-0.5 top-0.5 z-10 grid gap-0 text-[4.5px] font-black leading-[0.7] text-red-700">
           {jokerLetters.map((letter, index) => (
             <span key={`left-top-joker-${letter}-${index}`}>{letter}</span>
           ))}
         </span>
         <span
-          className="absolute left-1/2 top-1/2 block h-5 w-5 -translate-x-1/2 -translate-y-1/2 bg-contain bg-center bg-no-repeat"
+          className="absolute left-1/2 top-1/2 block h-3.5 w-3.5 -translate-x-1/2 -translate-y-1/2 bg-contain bg-center bg-no-repeat"
           style={{ backgroundImage: "url('/faolla-login-logo.png')" }}
         />
-        <span className="absolute bottom-0.5 right-0.5 grid rotate-180 gap-0 text-[4.5px] font-black leading-[0.7] text-red-700">
+        <span className="absolute bottom-0.5 right-0.5 z-10 grid rotate-180 gap-0 text-[4.5px] font-black leading-[0.7] text-red-700">
           {jokerLetters.map((letter, index) => (
             <span key={`left-bottom-joker-${letter}-${index}`}>{letter}</span>
           ))}
         </span>
       </span>
       <span className={`${cardClass} left-4 top-1 rotate-[10deg]`}>
-        <span className="absolute left-0.5 top-0.5 grid gap-0 text-[4.5px] font-black leading-[0.7] text-red-700">
+        <span className="absolute left-0.5 top-0.5 z-10 grid gap-0 text-[4.5px] font-black leading-[0.7] text-red-700">
           {jokerLetters.map((letter, index) => (
             <span key={`right-top-joker-${letter}-${index}`}>{letter}</span>
           ))}
         </span>
         <span
-          className="absolute left-1/2 top-1/2 block h-5 w-5 -translate-x-1/2 -translate-y-1/2 bg-contain bg-center bg-no-repeat"
+          className="absolute left-1/2 top-1/2 block h-3.5 w-3.5 -translate-x-1/2 -translate-y-1/2 bg-contain bg-center bg-no-repeat"
           style={{ backgroundImage: "url('/faolla-login-logo.png')" }}
         />
-        <span className="absolute bottom-0.5 right-0.5 grid rotate-180 gap-0 text-[4.5px] font-black leading-[0.7] text-red-700">
+        <span className="absolute bottom-0.5 right-0.5 z-10 grid rotate-180 gap-0 text-[4.5px] font-black leading-[0.7] text-red-700">
           {jokerLetters.map((letter, index) => (
             <span key={`right-bottom-joker-${letter}-${index}`}>{letter}</span>
           ))}
