780c1c0f2e810a5123820b7b04d68f4cc247da04
felix <caimin00x@gmail.com>
2026-07-16 08:54:21 +0200

    Show admin loading state during startup

--- src/app/admin/AdminClientLoader.tsx
+++ src/app/admin/AdminClientLoader.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useI18n } from "@/components/I18nProvider";
+import LoadingProgressScreen from "@/components/LoadingProgressScreen";
 import type { AdminClientProps } from "./AdminClient";
+
+function AdminClientLoading() {
+  const { locale } = useI18n();
+  return <LoadingProgressScreen locale={locale} />;
+}
 
 const AdminClient = dynamic(() => import("./AdminClient"), {
   ssr: false,
-  loading: () => null,
+  loading: () => <AdminClientLoading />,
 });
 
 export default function AdminClientLoader(props: AdminClientProps) {
